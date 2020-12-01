914f36f7079883c9645cec28ec14dbf2d886603a
DEft <daniel@eftodi.se>
2020-12-02 00:49:06 +0100

    Update macos build script

--- neo/cmake-macos-vulkan-debug.sh
+++ neo/cmake-macos-vulkan-debug.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
+
+#Change dir to script-dir
+cd "$(dirname "$0")"
+
+#remove old build-dir and make a new one
 cd ..
 rm -rf build
 mkdir build
 cd build
-cmake -DOPENAL_LIBRARY=/usr/local/opt/openal-soft/lib/libopenal.dylib -DOPENAL_INCLUDE_DIR=/usr/local/opt/openal-soft/include -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DSDL2=ON -DUSE_VULKAN=ON -DSPIRV_SHADERC=OFF ../neo -Wno-dev -Wexpansion-to-defined
+
+#execute cmake ...
+cmake -DOPENAL_LIBRARY=/usr/local/opt/openal-soft/lib/libopenal.dylib -DOPENAL_INCLUDE_DIR=/usr/local/opt/openal-soft/include -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DSDL2=ON -DUSE_VULKAN=ON -DSPIRV_SHADERC=OFF ../neo 
+#-Wno-dev -Wexpansion-to-defined
