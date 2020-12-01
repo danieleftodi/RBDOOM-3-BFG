36f557b5097d52316b74aba67dc64fd050a006fc
DEft <daniel@eftodi.se>
2020-12-02 00:56:53 +0100

    Fix as per what MoridinBG said in post (see link)
    https://github.com/RobertBeckebans/RBDOOM-3-BFG/issues/296#issuecomment-268970959

--- neo/sys/posix/posix_public.h
+++ neo/sys/posix/posix_public.h
@@ -68,7 +68,7 @@ char*		Posix_ConsoleInput();
 double 		MeasureClockTicks();
 
 #ifdef __APPLE__
-enum clk_id_t { CLOCK_REALTIME, CLOCK_MONOTONIC, CLOCK_MONOTONIC_RAW };
+enum clk_id_t { CLOCK__REALTIME, CLOCK__MONOTONIC, CLOCK__MONOTONIC_RAW };
 int clock_gettime( clk_id_t clock, struct timespec* tp );
 #endif
 
