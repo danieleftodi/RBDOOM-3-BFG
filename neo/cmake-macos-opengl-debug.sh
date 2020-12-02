a6730307f8a0cf143b7a08c297b3bf6c1fd89e47
DEft <daniel@eftodi.se>
2020-12-02 13:23:54 +0100

    Add OpenGL Build Scripts

---
+++ neo/cmake-macos-opengl-debug.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Change dir to script-dir
+cd "$(dirname "$0")"
+
+#remove old build-dir and make a new one
+cd ..
+rm -rf build
+mkdir build
+cd build
+
+#execute cmake ...
+cmake -DOPENAL_LIBRARY=/usr/local/opt/openal-soft/lib/libopenal.dylib -DOPENAL_INCLUDE_DIR=/usr/local/opt/openal-soft/include -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DSDL2=ON -DUSE_VULKAN=OFF -DSPIRV_SHADERC=OFF ../neo 
+#-Wno-dev -Wexpansion-to-defined
